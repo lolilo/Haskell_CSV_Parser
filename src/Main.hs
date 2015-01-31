f08ae31838e4e1f98a405e9ffa0db343f26e98fe
Liana Lo <liana.lo@prezi.com>
2015-01-31 21:26:05 +0100

    [refactor] CSVParser module

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,8 @@
 module Main where
 
-import qualified Data.ByteString.Lazy as BL
-
-import Data.Csv.Streaming
-import qualified Data.Foldable as F
-
--- type alias for data
-type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
-
-baseballStats :: BL.ByteString -> Records BaseballStats
-baseballStats = decode NoHeader
-
-fourth :: (a, b, c, d) -> d
-fourth (_, _, _, d) = d
+import CSVParser
 
 main :: IO ()
-main = do 
-        csvData <- BL.readFile "batting.csv"
-        let summed = F.foldr summer 0 (baseballStats csvData)
-        putStrLn $ "Total atBats was: " ++ (show summed)
-        where summer = (+) . fourth
+main = do
+    summed <- getAtBatsSum "batting.csv"
+    putStrLn $ "Total atBats was: " ++ (show summed)
