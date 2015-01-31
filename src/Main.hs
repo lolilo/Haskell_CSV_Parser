cef5c48e84479303eefd3efa280b8bfedeb9b8a1
Liana Lo <liana.lo@prezi.com>
2015-01-31 18:30:52 +0100

    Streaming - memory optimization

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,14 @@
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.Vector as V
 
-import Data.Csv
+import Data.Csv.Streaming
+import qualified Data.Foldable as F
 
 -- type alias for data
 type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
 
-baseballStats :: BL.ByteString -> Either String (V.Vector BaseballStats)
+baseballStats :: BL.ByteString -> Records BaseballStats
 baseballStats = decode NoHeader
 
 fourth :: (a, b, c, d) -> d
@@ -17,6 +17,6 @@ fourth (_, _, _, d) = d
 main :: IO ()
 main = do 
         csvData <- BL.readFile "batting.csv"
-        let summed = fmap (V.foldr summer 0) (baseballStats csvData)
+        let summed = F.foldr summer 0 (baseballStats csvData)
         putStrLn $ "Total atBats was: " ++ (show summed)
         where summer = (+) . fourth
